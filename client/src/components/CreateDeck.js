0511d182fb83ef45225e1a598d485094203e3eb7
Armando Ramirez <armandoramirez568@gmail.com>
2024-02-09 22:36:52 -0800

    Small changes to form and create deck

--- client/src/components/CreateDeck.js
+++ client/src/components/CreateDeck.js
@@ -1,51 +1,65 @@
-import './CreateDeck.css';
-import { useState } from 'react';
-import FormInput from './FormInput';
-
+import "./CreateDeck.css";
+import { useState } from "react";
+import FormInput from "./FormInput";
+import axios from "axios";
 
 function CreateDeck() {
-    //Define state for the my deck object
-    const [deck, setDeck] = useState({
-        title: "",
-        description: "",
+  //Define state for the my deck object
+  const [deck, setDeck] = useState({
+    title: "",
+    description: "",
+  });
+
+  //Define input fields with props
+  const inputs = [
+    {
+      id: 1,
+      name: "title",
+      type: "text",
+      placeholder: "Bio 120 Midterm",
+      errorMessage:
+        "Title should be between 1 and 20 characters and cannot include any special character.",
+      pattern: "^[A-Za-z0-9 ]{1,20}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "description",
+      type: "text",
+      placeholder: "These are my study flashcards for my midterm.",
+      className: "description",
+      pattern: "^[A-Za-z0-9 ]{0,50}$",
+      errorMessage: "Description should not exceed 50 characters.",
+    },
+  ];
+
+  //Update deck state when input value changes
+  const onChange = (e) => {
+    setDeck({ ...deck, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3001/decks", data).then((res) => {
+      console.log("Works!");
     });
+  };
 
-    //Define input fields with props
-    const inputs = [
-        {
-            id: 1,
-            name: "title",
-            type: "text",
-            placeholder: "Bio 120 Midterm",
-        },
-        {
-            id: 2,
-            name: "description",
-            type: "text",
-            placeholder: "These are my study flashcards for my midterm.",
-            className: "description"
-        },
-    ]
-
-    //Update deck state when input value changes
-    const onChange = (e) => {
-        setDeck({ ...deck, [e.target.name]: e.target.value })
-    }
-
-    console.log(deck);
-
-    return (
-        <div>
-            <h1>Create a New Deck</h1>
-            <form>
-                {inputs.map((input) => (
-                    <FormInput key={input.id} {...input} value={deck[input.name]} onChange={onChange} />
-                ))}
-
-            <button>Create</button>  
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Create a New Deck</h1>
+      <form onSubmit={onSubmit}>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={deck[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  );
 }
 
-export default CreateDeck
+export default CreateDeck;
--- client/src/components/FormInput.js
+++ client/src/components/FormInput.js
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import "./FormInput.css";
 
 function FormInput(props) {
 
+    const [focused, setFocused] = useState (false);
+
     //Destructure props to extract label, onChange and id
-    const { onChange, id, ...inputProps } = props
+    const { onChange, id, errorMessage, ...inputProps } = props
+
+    const handleFocus = (e) => {
+        setFocused(true);
+    }
 
     return (
         <div className="formInput">
-            <input {...inputProps} onChange={onChange} />
+            <input 
+                {...inputProps} 
+                onChange={onChange} 
+                onBlur={handleFocus} 
+                focused={focused.toString()}/>
+            <span>{errorMessage}</span>
         </div>
     )
 }
